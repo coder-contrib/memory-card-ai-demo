34132c4c3cf9244bc4a3775037f6510e67d56ec2
jason-instruqt <jason@instruqt.com>
2026-06-15 09:27:57 -0400

    Add red diamond to back of cards

--- src/App.jsx
+++ src/App.jsx
@@ -169,7 +169,7 @@ const MemoryGame = () => {
                 e.currentTarget.style.transform = 'scale(1)';
               }}
             >
-              {isCardVisible(index, card.symbol) ? card.symbol : '?'}
+              {isCardVisible(index, card.symbol) ? card.symbol : <div style={{width: '60px', height: '60px', background: 'red', transform: 'rotate(45deg)'}}></div>}
             </div>
           ))}
         </div>
